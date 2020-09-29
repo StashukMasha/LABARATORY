aaa0df8b910c6aa59c6d12728da802a3354fc519
StashukMasha <71558867+StashukMasha@users.noreply.github.com>
2020-09-29 10:16:04 +0300

    Update lab4var2.cpp

--- lab4var2.cpp
+++ lab4var2.cpp
@@ -1,56 +1,59 @@
 #include <stdio.h>
 
-const int n = 5;
+#define ARRAY_SIZE 5
 
-void vvodMassiv(int arr[]);
-void minPosition(int* min, int* position, int arr[]);
-int countFunction(int min, int position, int arr[]);
+void InputArray(int arr[]);
+int getMinArrayIndex(int arr[]);
+int getMultiply(int arr[]);
 
 int main()
 {
-	int arr[n];
-	vvodMassiv(arr);
-	int min = arr[0];
-	int position = 0;
-	minPosition(&min,&position,arr);
-	printf("result:%d", countFunction(min,position,arr));
+	int arr[ARRAY_SIZE];
+
+	InputArray(arr);
+	getMinArrayIndex(arr);
+	printf("result:%d", getMultiply(arr));
 
 	return 0;
 }
 
-void vvodMassiv(int arr[])
+void InputArray(int arr[])
 {
 	printf("vvedite massiv: ");
 
-	for (int i=0;i<n;i++)
+	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
 		scanf_s("%d", &arr[i]);
 	}
 }
 
-void minPosition(int* min, int* position, int arr[])
+int getMinArrayIndex(int arr[])
 {
-	for (int i = 1; i < n; i++)
+	int minIndex = 0;
+	int min = arr[minIndex];
+	for (int i = 1; i < ARRAY_SIZE; i++)
 	{
-		if (arr[i] < *min)
+		if (arr[i] < min)
 		{
-			*min = arr[i];
-			*position = i;
+			min = arr[i];
+			minIndex = i;
 		}
 
 	}
+	return minIndex;
 }
 
-int countFunction(int min, int position, int arr[])
+int getMultiply(int arr[])
 {
-	int proizvedenie = 1;
-	for (int i = 0; i < position; i++)
+	int min = arr[getMinArrayIndex(arr)];
+	int multiply = 1;
+	for (int i = 0; i < getMinArrayIndex(arr); i++)
 	{
 		if (arr[i] < 2 * min)
 		{
-			proizvedenie *= arr[i];
+			multiply *= arr[i];
 		}
 	}
-	
-	return proizvedenie;
+
+	return multiply;
 }
